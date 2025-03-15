15eff4730b16c905af967624aab25a138ed45a00
Amin Khan <abdulaminkhanjio111@gmail.com>
2025-03-15 17:26:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 10000), // Use Render's dynamic port
   app: {
     keys: env.array('APP_KEYS'),
   },
